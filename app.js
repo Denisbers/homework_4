f53123c7f9d7344ea9fa938b3f721da60e63f59a
Denisbers <denis16031984@gmail.com>
2023-10-21 23:51:51 +0300

    repo1

---
+++ app.js
@@ -0,0 +1,11 @@
+let firstDigit = +prompt('Please, enter first digit');
+let secondDigit = +prompt('Please, enter second digit');
+let sum = firstDigit + secondDigit;
+let sub = firstDigit - secondDigit;
+let mult = firstDigit * secondDigit;
+let div = firstDigit / secondDigit;
+
+
+alert(`${firstDigit}+${secondDigit}=${sum}\n${firstDigit}-${secondDigit}=${sub}\n${firstDigit}*${secondDigit}=${mult}\n${firstDigit}/${secondDigit}=${div}`);
+
+
